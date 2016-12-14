1a32aeea8fd7067e982b895f4546697cfc38128e
Predrag Rakic <prakic@renderedtext.com>
2016-12-14 17:09:09 +0100

    Move Exec class

--- lib/pipelines.rb
+++ lib/pipelines.rb
@@ -1,4 +1,5 @@
 require "pipelines/version"
+require "pipelines/exec"
 require "psych"
 
 module Pipelines
@@ -14,42 +15,6 @@ module Pipelines
     project = Psych.load_file(file)
   end
 
-  class Exec
-    def initialize(actions, name)
-      @name = name
-      @cmd  = actions.select {|a| a.key? "cmd"}.first["cmd"]
-      @test = actions.select {|a| a.key? "test"}.first["test"]
-      @cmd_results = []
-      @test_results = []
-    end
-
-    def cmd
-      @cmd
-    end
-
-    def run
-      run_cmd(@cmd, @cmd_results, true)
-      return if @cmd_results.last.last != 0
-      run_cmd(@test, @test_results, false)
-    end
-
-    def run_cmd(commands, results, should_break)
-      commands.each {|c|
-        begin
-          results << [%x[#{c}], $?.exitstatus]
-          break if should_break and $?.exitstatus != 0
-        rescue => e
-          results << [e, $?.exitstatus]
-          break if should_break
-        end
-      }
-      @results
-    end
-
-    def show
-      [{exec_name: @name, cmd: @cmd, test: @test}, @cmd_results, @test_results]
-    end
-  end
 
   class Pipeline
     def initialize(actions)
